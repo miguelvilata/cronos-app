6b61a4118f4c86dafa94fc7f4066cc25777d2b87
Miguel Uve <jm.vilata@yurest.com>
2024-05-25 17:16:24 +0200

    Working in view & coponents

--- app/src/main/java/com/example/roomcronoapp/views/AddView.kt
+++ app/src/main/java/com/example/roomcronoapp/views/AddView.kt
@@ -1,10 +1,50 @@
 package com.example.roomcronoapp.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.roomcronoapp.components.FloatButton
+import com.example.roomcronoapp.components.MainIconButton
+import com.example.roomcronoapp.components.MainTitle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddView(navController: NavController) {
-    Text("Ad View")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { MainTitle(title = "ADD CRONO APP") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                ),
+                navigationIcon = {
+                    MainIconButton(icon = Icons.Default.ArrowBack) {
+                        navController.popBackStack()
+                    }
+                }
+            )
+        },
+    ) {
+        ContentAddView(it, navController)
+    }
+}
+
+@Composable
+fun ContentAddView(it: PaddingValues, navController: NavController) {
+    Column (
+        modifier = Modifier.padding(it)
+    ) {
+
+    }
 }
--- app/src/main/java/com/example/roomcronoapp/views/HomeView.kt
+++ app/src/main/java/com/example/roomcronoapp/views/HomeView.kt
@@ -1,10 +1,47 @@
 package com.example.roomcronoapp.views
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.roomcronoapp.components.MainTitle
+import com.example.roomcronoapp.components.FloatButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(navController: NavController) {
-    Text("Hola")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { MainTitle(title = "CRONO APP") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatButton {
+                navController.navigate("AddView")
+            }
+        }
+    ) {
+        ContentHomeView(it, navController)
+    }
+}
+
+@Composable
+fun ContentHomeView(it: PaddingValues, navController: NavController) {
+    Column (
+        modifier = Modifier.padding(it)
+    ) {
+
+    }
 }
