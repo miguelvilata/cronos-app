24a2ecd78a09467228c184863b5a545f1de40098
Miguel Uve <jm.vilata@yurest.com>
2024-05-25 08:06:40 +0200

    Base navigation & views

---
+++ app/src/main/java/com/example/roomcronoapp/views/AddView.kt
@@ -0,0 +1,10 @@
+package com.example.roomcronoapp.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AddView(navController: NavController) {
+    Text("Ad View")
+}
